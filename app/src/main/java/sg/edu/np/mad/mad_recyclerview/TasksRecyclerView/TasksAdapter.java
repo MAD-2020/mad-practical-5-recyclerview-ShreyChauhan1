01a814fc6aaffaea0565d99875e5bde6c9f9bacf
Shrey Chauhan <s10197306@connect.np.edu.sg>
2020-05-27 23:09:00 +0800

    Week 5 update

--- app/src/main/java/sg/edu/np/mad/mad_recyclerview/TasksRecyclerView/TasksAdapter.java
+++ app/src/main/java/sg/edu/np/mad/mad_recyclerview/TasksRecyclerView/TasksAdapter.java
@@ -71,6 +71,7 @@ public class TasksAdapter extends RecyclerView.Adapter<TaskViewHolder>{
                           @Override
                           public void onClick(DialogInterface dialog, int which) {
                               data.remove(position);
+                              notifyDataSetChanged();
                           }
                       });
               AlertDialog dialog=builder.create();
